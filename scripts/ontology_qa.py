87b0757ed6548e90cddeff3deac2a5601a2b816d
otellomaria <otello.roscioni@semanticpartners.com>
2025-09-22 15:27:24 +0100

    regex expressions for namespace hijacking reverted

--- scripts/ontology_qa.py
+++ scripts/ontology_qa.py
@@ -528,10 +528,19 @@ WHERE {
   }
   # Detect if the class URI starts with a known external namespace
   FILTER (
-    regex(STR(?resource), "^http://www.w3.org/") ||
-    regex(STR(?resource), "^http://purl.org/") ||
-    regex(STR(?resource), "^http://xmlns.com/") ||
-    regex(STR(?resource), "^http://www.linkedmodel.org/")
+    regex(STR(?resource), "^http://www.w3.org/1999/02/22-rdf-syntax-ns") ||
+    regex(STR(?resource), "^http://www.w3.org/2000/01/rdf-schema") ||
+    regex(STR(?resource), "^http://www.w3.org/2001/XMLSchema") ||
+    regex(STR(?resource), "^http://www.w3.org/2002/07/owl") ||
+    regex(STR(?resource), "^http://www.w3.org/2004/02/skos/core") ||
+    regex(STR(?resource), "^http://www.w3.org/ns/shacl") ||
+    regex(STR(?resource), "^http://www.w3.org/XML/1998/namespace") ||
+    regex(STR(?resource), "^http://purl.org/dc/terms") ||
+    regex(STR(?resource), "^http://purl.org/dc/elements/1.1") ||
+    regex(STR(?resource), "^http://purl.org/vocab/vann") ||
+    regex(STR(?resource), "^http://purl.org/ontology/bibo/status") ||
+    regex(STR(?resource), "^http://xmlns.com/foaf/0.1") ||
+    regex(STR(?resource), "^http://www.linkedmodel.org/1.2/schema/vaem")
   )
 }
 """
@@ -541,14 +550,23 @@ WHERE {
 hijacking = """
 SELECT DISTINCT ?resource
 WHERE {
-  VALUES ?type { owl:Class rdfs:Class }
+  VALUES ?type { owl:Class rdfs:Class owl:ObjectProperty rdf:Property owl:DatatypeProperty sh:NodeShape sh:PropertyShape }
   ?resource a ?type .
   # Detect if the class URI starts with a known external namespace
   FILTER (
-    regex(STR(?resource), "^http://www.w3.org/") ||
-    regex(STR(?resource), "^http://purl.org/") ||
-    regex(STR(?resource), "^http://xmlns.com/") ||
-    regex(STR(?resource), "^http://www.linkedmodel.org/")
+    regex(STR(?resource), "^http://www.w3.org/1999/02/22-rdf-syntax-ns") ||
+    regex(STR(?resource), "^http://www.w3.org/2000/01/rdf-schema") ||
+    regex(STR(?resource), "^http://www.w3.org/2001/XMLSchema") ||
+    regex(STR(?resource), "^http://www.w3.org/2002/07/owl") ||
+    regex(STR(?resource), "^http://www.w3.org/2004/02/skos/core") ||
+    regex(STR(?resource), "^http://www.w3.org/ns/shacl") ||
+    regex(STR(?resource), "^http://www.w3.org/XML/1998/namespace") ||
+    regex(STR(?resource), "^http://purl.org/dc/terms") ||
+    regex(STR(?resource), "^http://purl.org/dc/elements/1.1") ||
+    regex(STR(?resource), "^http://purl.org/vocab/vann") ||
+    regex(STR(?resource), "^http://purl.org/ontology/bibo/status") ||
+    regex(STR(?resource), "^http://xmlns.com/foaf/0.1") ||
+    regex(STR(?resource), "^http://www.linkedmodel.org/1.2/schema/vaem")
   )
 }
 """
@@ -997,7 +1015,7 @@ def main():
     sep()
 
     qan = qa_check_results("PropertyShapes with the same label",qan)
-    results = g.query(node_shape_same_label)
+    results = g.query(property_shape_same_label)
     if not results:
         print("PASS - No PropertyShape share the same label.")
         qa_metrics['nonUniquePSLabels'] = 0
