128e74ec032b41e10d17f0b9ec858bc39830f73f
otellomaria <otello.roscioni@semanticpartners.com>
2026-01-12 06:52:16 +0000

    bugfix in path checking

--- scripts/ontology_qa.py
+++ scripts/ontology_qa.py
@@ -125,8 +125,9 @@ def qa_terminate(file, log):
         log (str): Results of the Quality Assurance tests, formatted in markdown.
     """
     # Print output to file or STDOUT.
-    os.makedirs(os.path.dirname(file) or '.', exist_ok=True) # Ensure directory exists
     if file:
+         # Ensure directory exists
+        os.makedirs(os.path.dirname(file) or '.', exist_ok=True)
         with open(file, "w", encoding="utf-8") as f:
             f.write(log)
     else:
