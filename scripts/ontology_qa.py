376ae2c909fe691a21176d8d864e45929861ca9d
otellomaria <otello.roscioni@semanticpartners.com>
2026-04-20 05:22:49 +0100

    Missing Domain and Range check renamed

--- scripts/ontology_qa.py
+++ scripts/ontology_qa.py
@@ -1307,7 +1307,7 @@ def check_isolated_classes(in_metrics, graph, name, c, status, verbose):
     log += sep()
     return metrics, violations, test, log, c, status
 
-def check_missing_dr_property(in_metrics, graph, name, c, status, verbose):
+def check_property_missing_dr(in_metrics, graph, name, c, status, verbose):
     """
     QA test checking properties for rdfs:domain or rdfs:range declaration.
     
@@ -1765,6 +1765,7 @@ def main():
     parser.add_argument('--ctrf-dir', type=str, metavar='directory', default='ctrf', help='Directory to write CTRF report to.')
     parser.add_argument('--ctrf-filename', type=str, metavar='filename', default=None, help='Filename for CTRF report (if None, uses default pattern).')
     parser.add_argument('-o', '--output', type=str, metavar='filename', help='Output file name (optional). If omitted, print to stdout.')
+    parser.add_argument('-c', '--config', type=str, metavar='.rdf-lint.yml', help='Path to .rdf-lint.yml configuration file to enable or disable individual checks.')
     parser.add_argument('data_files', nargs='+', help='List of RDF files or folders to process.')
     args = parser.parse_args()
 
@@ -1841,7 +1842,7 @@ def main():
         (True, check_node_shape_same_label,          "NodeShapes with the same label"          ),
         (True, check_property_shape_same_label,      "PropertyShapes with the same label"      ),
         (True, check_isolated_classes,               "Isolated classes"                        ),
-        (True, check_missing_dr_property,            "Missing Domain or Range in Properties"   ),
+        (True, check_property_missing_dr,            "Missing Domain or Range in Properties"   ),
         (True, check_unique_identifiers,             "Non-unique identifiers"                  ),
         (True, check_subclass_cycles,                "Including Cycles in a Class Hierarchy"   ),
         (True, check_untyped_class,                  "Untyped Classes"                         ),
